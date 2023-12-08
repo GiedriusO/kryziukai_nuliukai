949122deb767d33d115cc5d31056f9bfbbb05c83
Giedr <Giedriuspgg@gmail.com>
2023-12-08 08:49:24 +0200

    bandau

---
+++ kryziukai_nuliukai.py
@@ -0,0 +1,5 @@
+# Sukurti kryžiukų/nuliukų žaidimą, kuris:
+# Leistų žaisti dviems žaidėjams (X ir O)
+# Teisingai fiksuotų vieno iš žaidėjų laimėjimą arba lygiasias ir stabdytų žaidimą
+# Žaidimas vyktų konsolėje, grafinio interfeiso nereikia (bet galima daryti, tada konsolės nebereikia)
+# Sukurtą žaidimą paskelbti github repozitorijoje, nuorodą paskelbti teamsuose, tam skirtoje užduotyje (Assignments)
