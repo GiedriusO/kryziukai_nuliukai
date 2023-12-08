7b91dcee740d4d62827a456abfa433263d975811
Giedr <Giedriuspgg@gmail.com>
2023-12-08 08:51:07 +0200

    bandau

--- kryziukai_nuliukai.py
+++ kryziukai_nuliukai.py
@@ -3,3 +3,5 @@
 # Teisingai fiksuotų vieno iš žaidėjų laimėjimą arba lygiasias ir stabdytų žaidimą
 # Žaidimas vyktų konsolėje, grafinio interfeiso nereikia (bet galima daryti, tada konsolės nebereikia)
 # Sukurtą žaidimą paskelbti github repozitorijoje, nuorodą paskelbti teamsuose, tam skirtoje užduotyje (Assignments)
+
+print("Hello world")
