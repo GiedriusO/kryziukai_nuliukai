b7f49981e8cc7078fe68e1490f5192856a339e65
Giedr <Giedriuspgg@gmail.com>
2023-12-08 09:46:25 +0200

    bandau

--- kryziukai_nuliukai.py
+++ kryziukai_nuliukai.py
@@ -10,20 +10,24 @@
 # 5 tikrinti ar laimejo/lygiosios ir uzbaigti loop
 
 lentele = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+zaidejas = "x"
 
 def print_lentele():
     print(lentele[0] + "|" + lentele[1] + "|" + lentele[2])
     print(lentele[3] + "|" + lentele[4] + "|" + lentele[5])
     print(lentele[6] + "|" + lentele[7] + "|" + lentele[8])
 
-
-zaidejas = "x"
-
 def zaidejo_input():
     inputas = int(input("iveskite skaiciu nuo 1 iki 9"))
     if inputas <= 1 or inputas <= 9:
         lentele[inputas-1] = zaidejas
 
+def tikrinti_vertikalei():
+    if lentele[0]==lentele[3]==lentele[6] or lentele[1]==lentele[4]==lentele[7] or lentele[2]==lentele[5]==lentele[8]:
+        print("sveikiname jus laimejote")
+
+
 while True:
     print(print_lentele())
     zaidejo_input()
+    tikrinti_vertikalei()
